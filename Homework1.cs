556054935f85b2fb07504aceb5369a011995a49a
EthanKlein225 <143645091+EthanKlein225@users.noreply.github.com>
2023-08-30 13:38:24 -0500

    Upload Homework1

---
+++ Homework1.cs
@@ -0,0 +1,14 @@
+﻿namespace Homework1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        float X = 2.5f;
+        float Y = 3.3f;
+        float Z;
+        Z = (4*X*X)+(3*Y);
+        float answer = Z;
+        Console.WriteLine("X = " + X + ", Y = " + Y + "\nThe value of Z is: " + answer);
+    }
+}
