4b55593498b3ac84c704e0caaf8d160bb560e3c9
EthanKlein225 <143645091+EthanKlein225@users.noreply.github.com>
2023-09-20 21:48:26 -0500

    Homework 2 Corrected *LOOK AT THIS ONE*

--- Homework2.cs
+++ Homework2.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
+        //Question 1
+        Console.WriteLine("Please input a letter grade: ");
+        string letter = Console.ReadLine();
+        if(letter=="A"){
+            Console.WriteLine("GPA Point: 4");
+        }
+        else if(letter=="B"){
+            Console.WriteLine("GPA Point: 3");
+        }
+        else if(letter=="C"){
+            Console.WriteLine("GPA Point: 2");
+        }
+        else if(letter=="D"){
+            Console.WriteLine("GPA Point: 1");
+        }
+        else if(letter=="F"){
+            Console.WriteLine("GPA Point: 0");
+        }
+        else{
+            Console.WriteLine("Wrong Letter Grade!");
+        }
+        
+        
+        //Question 2
         Console.WriteLine("Please input the first num: ");
         string n1_str = Console.ReadLine();
         int n1 = Convert.ToInt16(n1_str);
